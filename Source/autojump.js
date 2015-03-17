880fc0ebb64a1bcc69527d150b79d3e2bfc0cb0d
zghaia <zghaia@gmail.com>
2015-03-17 22:08:31 +0800

    add source

---
+++ Source/autojump.js
@@ -0,0 +1,17 @@
+﻿function autoJump(id,url)
+{
+		var urlFrom = localStorage["from"];
+    var urlTo = localStorage["to"];
+    
+    if(!urlFrom || !urlTo)
+    {
+    	urlFrom="";
+    	urlTo="";
+    }
+    else
+    {
+			url=url.replace(urlFrom,urlTo);
+	  	chrome.tabs.remove(id);
+    	chrome.tabs.create({url:url});
+    }
+}
